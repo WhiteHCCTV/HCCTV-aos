3a5f48571e672e282ae93fe54a958d339168fd20
k906506 <k906506@naver.com>
2022-04-14 11:19:42 +0900

    🏗️ [feat] BaseActivity, BaseFragment, BaseViewModel 구현

---
+++ app/src/main/java/com/example/hcctv/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.example.hcctv.base
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    abstract val FRAGMENT_TAG: String
+
+    private var _binding: T? = null
+    val binding get() = _binding ?: error("Not Initialized")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = this@BaseFragment
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/hcctv/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.hcctv.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel: ViewModel() {
+}
