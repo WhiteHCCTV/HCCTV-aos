b19ad458af4f77b7e3705aa9f0b3e9305a680a20
k906506 <k906506@naver.com>
2022-05-09 17:16:30 +0900

    🏗 [feat] Device Model 관련 Repository 정의

--- app/src/main/java/com/example/hcctv/model/repository/DeviceRepository.kt
+++ app/src/main/java/com/example/hcctv/model/repository/DeviceRepository.kt
@@ -3,11 +3,11 @@ package com.example.hcctv.model.repository
 import com.example.hcctv.model.data.Device
 
 interface DeviceRepository {
-    suspend fun insertDevice(device: Device)
+    suspend fun insertDevice(address : String)
 
-    suspend fun updateDevice(device: Device)
+    suspend fun updateDevice(address : String)
 
-    suspend fun deleteDevice(device: Device)
+    suspend fun deleteDevice(address : String)
 
-    suspend fun getAllDevices(): List<Device>
+    fun getAllDevices(): List<Device>
 }
--- app/src/main/java/com/example/hcctv/model/repository/DeviceRepositoryImpl.kt
+++ app/src/main/java/com/example/hcctv/model/repository/DeviceRepositoryImpl.kt
@@ -6,20 +6,18 @@ import com.example.hcctv.model.database.DeviceDao
 import com.example.hcctv.model.database.DeviceDatabase
 
 class DeviceRepositoryImpl(application: Application) : DeviceRepository {
-    private val deviceDao: DeviceDao
-    private val allDevices: List<Device>
-
-    init {
-        val db: DeviceDatabase = DeviceDatabase.getInstance(application)!!
-        deviceDao = db.deviceDao()
-        allDevices = db.deviceDao().getAllDevices()
+    private val deviceDao by lazy {
+        DeviceDatabase.getInstance(application)!!.deviceDao()
     }
 
-    override suspend fun insertDevice(device: Device) = deviceDao.insertDevice(device)
+    override suspend fun insertDevice(address: String) =
+        deviceDao.insertDevice(Device(id = null, ipAddress = address))
 
-    override suspend fun updateDevice(device: Device) = deviceDao.updateDevice(device)
+    override suspend fun updateDevice(address: String) =
+        deviceDao.updateDevice(Device(id = null, ipAddress = address))
 
-    override suspend fun deleteDevice(device: Device) = deviceDao.deleteDevice(device)
+    override suspend fun deleteDevice(address: String) =
+        deviceDao.deleteDevice(Device(id = null, ipAddress = address))
 
-    override suspend fun getAllDevices(): List<Device> = deviceDao.getAllDevices()
+    override fun getAllDevices(): List<Device> = deviceDao.getAllDevices()
 }
