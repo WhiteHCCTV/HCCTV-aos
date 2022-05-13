a7400176fcf73db409917c4e9bffe1abf7856a62
k906506 <k906506@naver.com>
2022-05-13 13:45:26 +0900

    🏗 [feat] 서버로부터 이미지 수신 후에 사용자가 선택하는 기능 구현

--- app/src/main/java/com/example/hcctv/view/fragment/DeviceDetailFragment.kt
+++ app/src/main/java/com/example/hcctv/view/fragment/DeviceDetailFragment.kt
@@ -1,82 +1,192 @@
 package com.example.hcctv.view.fragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.hcctv.R
+import com.example.hcctv.adapter.ImageItemAdapter
 import com.example.hcctv.base.BaseFragment
 import com.example.hcctv.databinding.FragmentDeviceDetailBinding
-import io.socket.client.IO
+import com.example.hcctv.model.data.Image
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 import java.lang.Exception
+import java.net.Socket
+import java.net.UnknownHostException
 
 class DeviceDetailFragment(override val FRAGMENT_TAG: String = "DEVICE_DETAIL_FRAGMENT") :
     BaseFragment<FragmentDeviceDetailBinding>(R.layout.fragment_device_detail) {
+    private val imageItemList: MutableList<Image> = mutableListOf()
+    private val selectedItemIndex: MutableList<Int> = mutableListOf()
 
-    private val socket by lazy {
-        IO.socket(ipAddress)
+    private val adapter by lazy {
+        ImageItemAdapter(itemClickListener = {
+            if (selectedItemIndex.contains(it)) {
+                selectedItemIndex.remove(it)
+            } else {
+                selectedItemIndex.add(it)
+            }
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity.hideBottomNavi(true)
 
-        connectSocket()
-        sendMessage()
-        receiveMessage()
+        initViews()
+        saveSelectedItems()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
         activity.hideBottomNavi(false)
-        socket.disconnect()
     }
 
-    private fun connectSocket() {
-        try {
-            socket.connect()
-            Toast.makeText(getActivity(), "소켓 연결 완료", Toast.LENGTH_SHORT).show()
-        } catch (e: Exception) {
-            e.printStackTrace()
+    private fun initViews() {
+        getFiles()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.imageRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.imageRecyclerView.adapter = adapter
+        adapter.submitList(imageItemList.toList())
+    }
+
+    private fun getFileCount(): Int {
+        var fileCount = 0
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val initSocket = Socket(IP_ADDRESS, PORT_NUMBER)
+                val inputStream = initSocket.getInputStream()
+                val dataInputStream = DataInputStream(inputStream)
+
+                fileCount = dataInputStream.readInt()
+
+                // UI 변경을 위해 메인 쓰레드로 전환
+                withContext(Dispatchers.Main) {
+                    activity.showToast(requireContext(), fileCount.toString())
+                }
+
+                dataInputStream.close()
+                inputStream.close()
+                initSocket.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+
+        return fileCount
     }
 
-    private fun sendMessage() {
-        binding.btnSend.setOnClickListener {
-            val message = binding.messageEditText.text.toString().trim()
-            binding.messageEditText.text = null
+    private fun getFiles() {
+        // Progress 보여주고 이미지 수신 완료되면 RecyclerView 보여주기
+        showProgress()
 
-            socket.emit("new message", message)
+        // 이미지 수신
+        val fileCount = getFileCount()
+        var count = 0
 
-            Toast.makeText(getActivity(), "전송 완료", Toast.LENGTH_SHORT).show()
-        }
-    }
+        var socket: Socket? = null
+        var fileOutputStream: FileOutputStream? = null
 
-    private fun receiveMessage() {
-        Toast.makeText(getActivity(), "대기 중", Toast.LENGTH_SHORT).show()
-        try {
-            socket.on("new message") { args ->
-                Toast.makeText(getActivity(), "수신 중", Toast.LENGTH_SHORT).show()
-                CoroutineScope(Dispatchers.Main).launch {
-                    val data = args[0]
-                    var message = ""
-                    try {
-                        message = data.toString()
-                    } catch (e: Error) {
-                        e.printStackTrace()
+        CoroutineScope(Dispatchers.IO).launch {
+            do {
+                try {
+                    socket = Socket(IP_ADDRESS, PORT_NUMBER)
+                    val inputStream = socket?.getInputStream()
+                    val filePath = "$PATH$count.jpg"
+                    val fileOutputStream = FileOutputStream(filePath)
+                    val dataBuffer = ByteArray(10000)
+                    val dataLength = inputStream?.read(dataBuffer)
+
+                    while (dataLength != -1) {
+                        // dataBuffer에 파일 쓰고
+                        fileOutputStream.write(dataBuffer, 0, dataLength!!)
+                        // dataBuffer 저장
+                        imageItemList.add(Image(id = count, image = dataBuffer, selected = false))
                     }
-                    Toast.makeText(getActivity(), "수신 완료\n$message", Toast.LENGTH_SHORT).show()
+
+
+                    withContext(Dispatchers.Main) {
+                        activity.showToast(requireContext(), filePath)
+                    }
+
+                } catch (e: UnknownHostException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } finally {
+                    fileOutputStream?.let {
+                        try {
+                            it.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    socket?.let {
+                        try {
+                            it.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+                count += 1
+            } while (count < fileCount)
+        }
+
+        hideProgress()
+    }
+
+    private fun saveSelectedItems() {
+        binding.btnSubmit.setOnClickListener {
+            val listener = DialogInterface.OnClickListener { _, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                        // 오름차순으로 정렬하고 submit
+                        selectedItemIndex.sort()
+                    }
+                    DialogInterface.BUTTON_NEGATIVE -> {
+
+                    }
                 }
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
+
+            AlertDialog.Builder(requireActivity()).apply {
+                setTitle("학습 사진 업로드")
+                setMessage("업로드 하시겠습니까?")
+                setPositiveButton("예", listener)
+                setNegativeButton("아니오", listener)
+                show()
+            }
         }
+    }
+
+    private fun showProgress() = with(binding) {
+        progressBar.visibility = View.VISIBLE
+        imageRecyclerView.visibility = View.INVISIBLE
+        btnSubmit.visibility = View.INVISIBLE
+    }
+
+    private fun hideProgress() = with(binding) {
+        progressBar.visibility = View.INVISIBLE
+        imageRecyclerView.visibility = View.VISIBLE
+        btnSubmit.visibility = View.VISIBLE
     }
 
     companion object {
-        const val ipAddress = "http://192.168.1.1:8000"
+        const val IP_ADDRESS = "http://192.168.1.1"
+        const val PORT_NUMBER: Int = 5555
+        const val PATH = "./sample/client"
     }
 }
